9ad180901575c3cfe67a8def130719a8e146e2d7
DevJhonaN <jhonatannegri659@gmail.com>
2026-08-25 21:44:47 -0300

    implement categories module

---
+++ src/cases/categories/category.module.ts
@@ -0,0 +1,13 @@
+import { Module } from "@nestjs/common";
+import { Category } from "./category.entity";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { CategoryController } from "./category.controller";
+import { CategoryService } from "./category.service";
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Category])],
+  controllers: [CategoryController],
+  providers: [CategoryService],
+})
+
+export class CategoryModule {}
